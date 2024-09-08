a2c1526357f8f6d198ae97bd3840b31bd6d47ae0
Yu Heng Su <yolanda.su00@gmail.com>
2024-09-08 00:07:15 -0400

    Completed Task 8

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+"""Server for the web deployment of this application."""
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,6 +7,7 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def sent_detector():
+    """Endpoint for running the emotion detector on the given text."""
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
 
@@ -15,16 +18,19 @@ def sent_detector():
     sadness_score = response['sadness']
     dominant_emotion = response['dominant_emotion']
 
-    if dominant_emotion == None:
+    if dominant_emotion is None:
         return "Invalid text! Please try again!"
-    else:
-        return "For the given statement, the system response is 'anger': {}, 'disgust': {},  \
-                'fear': {}, 'joy': {} and 'sadness': {}. The dominant emotion is {}." \
-                .format(anger_score, disgust_score, fear_score, joy_score, sadness_score, dominant_emotion)
+
+    return f"For the given statement, the system response is 'anger': {anger_score}, \
+            'disgust': {disgust_score}, 'fear': {fear_score}, \
+            'joy': {joy_score} and 'sadness': {sadness_score}. \
+            The dominant emotion is {dominant_emotion}."
 
 @app.route("/")
 def render_index_page():
+    """Index page of this application."""
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000) 
+    app.run(host="0.0.0.0", port=5000)
+    
